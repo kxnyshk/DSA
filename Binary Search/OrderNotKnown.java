4a80cd328762faf3783fe2be4708d253c6522854
kxnyshk <ikanishk1172@gmail.com>
2021-10-09 20:09:47 +0530

    Scan() method corrected. Extra test arrays deleted. Return values corrected.

--- Binary Search/OrderNotKnown.java
+++ Binary Search/OrderNotKnown.java
@@ -4,35 +4,16 @@ public class OrderNotKnown {
     
     static{
     
-        System.out.println("\nImplementing binary search for an unknown sort.");
+        System.out.println("\nBinary search [Sort order unknown]");
     }
+    private static int value;
     public static void main(String[] args){
     
-        int[] array1 = {1};
+        int[] array1 = {1,12,34,56,80};
         int n1 = array1.length;
 
-        int[] array2 = {1,1,1};
-        int n2 = array2.length;
-        
-        int[] array3 = {1,2,5,7};
-        int n3 = array3.length;
-
-        int[] array4 = {8,4,3,0};
-        int n4 = array4.length;
-
         printArray(array1);
-        System.out.println("\n" + binSearch(array1, n1));
-        printArray(array2);
-        System.out.println("\n" + binSearch(array2, n2));
-        printArray(array3);
-        System.out.println("\n" + binSearch(array3, n3));
-        printArray(array4);
-        System.out.println("\n" + binSearch(array4, n4));
-        
-        // return 0 --> single indices array
-        // return -1 --> value not found
-        // return -2 --> array with all duplicates values
-        // return -3 --> invalid binSearch()
+        System.out.print("\nIndex: " + binSearch(array1, n1));
     }
 
     private static void printArray(int[] array) {
@@ -43,18 +24,22 @@ public class OrderNotKnown {
     }
 
     private static int binSearch(int[] array, int n){
+        int start = 0;
+        int end = n-1;
+        
         if(n == 1){
-            return 0;
+            return start;
         }
 
-        else if(n>1){
-            if(array[0]<array[n-1]){
-                System.out.print("\nElement to search: ");
-                Scanner scan = new Scanner(System.in);
-                int value = scan.nextInt();
+        else if(array[start] == array[end]){
+            System.out.print("\nDuplicate values detected!\nIndex: " + start + " --> (to)");
+            return end;
+        }
 
-                int start = 0;
-                int end = n-1;
+        else{
+            Scan();
+            
+            if(array[start]<array[end]){
                 while(start<=end){   
                     int mid = (start+(end-start)/2);
                     if(value == array[mid]){
@@ -67,16 +52,9 @@ public class OrderNotKnown {
                         start = mid+1;
                     }
                 }
-                return -1;
             }
 
-            else if(array[0]>array[n-1]){
-                System.out.print("\nElement to search: ");
-                Scanner scan = new Scanner(System.in);
-                int value = scan.nextInt();
-
-                int start = 0;
-                int end = n-1;
+            else if(array[start]>array[end]){
                 while(start<=end){   
                     int mid = (start+(end-start)/2);
                     if(value == array[mid]){
@@ -89,13 +67,15 @@ public class OrderNotKnown {
                         end = mid-1;
                     }
                 }
-                return -1;
-            }
-
-            else if(array[0] == array[n-1]){
-                return -2;
             }
         }
-        return -3;
+        return -1;
+    }
+
+    private static void Scan() {
+        System.out.print("\nsearch: ");
+        Scanner scan = new Scanner(System.in);
+        value = scan.nextInt();
+        scan.close();
     }
 }
